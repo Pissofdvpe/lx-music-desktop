ecdc018fa1a24f99ab2292b1a60e37e9ac2081df
lyswhut <lyswhut@qq.com>
2026-06-04 20:54:55 +0800

    Linux 点击托盘时将显示主界面（#2840）

--- src/main/modules/tray.ts
+++ src/main/modules/tray.ts
@@ -150,7 +150,7 @@ export const createTray = () => {
   // tray.setToolTip('LX Music')
   // createMenu()
   tray.setIgnoreDoubleClickEvents(true)
-  if (isWin) {
+  if (!isMac) {
     tray.on('click', () => {
       showMainWindow()
     })
